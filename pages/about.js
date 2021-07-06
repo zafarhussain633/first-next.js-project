761c7cbb5cda78094e2760f389f6531e27635451
Zafar <37826183+zafarhussain633@users.noreply.github.com>
2021-07-06 10:34:31 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-
+import styles from '../styles/Home.module.css'
 
 const About = ()=>{
     return (
@@ -8,8 +8,8 @@ const About = ()=>{
             <title>simple next.js app by_zafar</title>
             <meta name="keyword" content="next.js"/>
             </Head>
-            <h1>about page</h1>
-            this is about page 
+            <h1 className={styles.title}>about page</h1>
+            <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur dolorum ipsum temporibus sint consequuntur eligendi nihil dicta debitis, vel officiis cupiditate quisquam aut magni consectetur necessitatibus doloribus fuga, error adipisci impedit sequi magnam earum neque quod. Officia repellendus, dolore enim aspernatur corporis, recusandae dignissimos, beatae libero aliquid deserunt vitae veniam.</p>
         </div>
     )
 }
